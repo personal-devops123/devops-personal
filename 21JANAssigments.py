316eb618d9956e4c0f53c3a0011eff582e65a76a
Dhananjaya0000 <dhananjaya.parida@gmail.com>
2021-01-23 14:12:36 +0530

    21st JAN assignment

---
+++ 21JANAssigments.py
@@ -0,0 +1,31 @@
+#WAP find sum of elements without using sum() function.
+
+
+#WAP find max elements without using max() function.
+
+#WAP find min elements without using min() function.
+
+#WAP accept number and deploy its index without index method.
+
+#WAP accept a element and count total no. of occurance without count() method.
+
+#WAP print above list elements in reverse order without using revers() method.
+
+
+
+#WAP store even numbers into one list and odd number in another list.
+
+
+#WAP accept a element and remove the given element from a lists without using remove () method.
+
+x=['unix','linux','aws','perl','python','java','perl','linux','devops']
+
+#WAP  print what ever elements ending with 's' chrectors
+
+#WAP  print what ever elements begining  with 'vowel' chrectors
+
+#WAP program whatever elements length more than 5 charecters.
+
+#WAP delete all duplicate elements.
+
+#WAP display only duplicate elements.
